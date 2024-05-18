2b6f4b8aa8f9f954b5bd965b8b489ed53c1c4af8
mahbubur3 <mahbubur9@yahoo.com>
2024-05-18 10:07:51 +0600

    initial commit

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     cout << "Hello world\n";
+
+//     return 0;
+// }
+
+
+int main() {
+    cout << "Hello world\n";
+    cout << "Hello cricket\n";
+
+    return 0;
+}
