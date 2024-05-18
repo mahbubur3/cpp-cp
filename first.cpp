b3c72f4600a954d87e00b716998829085ed34da6
mahbubur3 <mahbubur9@yahoo.com>
2024-05-18 10:29:22 +0600

    updated

--- first.cpp
+++ first.cpp
@@ -1,16 +1,103 @@
 #include <iostream>
 using namespace std;
 
+
 // int main() {
-//     cout << "Hello world\n";
+//     cout << "Hello world\n"; //* cout - see-out
 
 //     return 0;
 // }
 
 
-int main() {
-    cout << "Hello world\n";
-    cout << "Hello cricket\n";
+// int main() {
+//     cout << "Hello world\n";
+//     cout << "Hello cricket\n";
+
+//     return 0;
+// }
+
+
+// int main() {
+//     cout << "United Emirates\n";
+
+//     return 0;
+// }
+
+
+/* int main() {
+    // int num = 20;
+    // cout << num;
+
+    // int num;
+    // num = 20;
+    // cout << num;
+
+    int num = 20;
+    num = 10;
+    cout << num;
 
     return 0;
-}
+} */
+
+
+/*  int main() {
+    int myAge = 23;
+    int myRank = 10;
+
+    cout << "I am " << myAge << " years old.\n";
+    cout << "My rank is " << myRank;
+
+    return 0;
+ } */
+
+
+/*  int main() {
+    int x = 10, y = 20;
+    int sum = x + y;
+    cout << sum;
+    
+    return 0;
+ } */
+
+
+//! USER INPUT
+/* int main() {
+    int n;
+    cin >> n; //* cin - see-in
+    cout << n;
+
+    return 0;
+} */
+
+
+/* int main() {
+    int n;
+    cin >> n;
+    cout << "My number is " << n;
+
+    return 0;
+} */
+
+
+/* int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    int result = a + b;
+    cout << "Summation = " << result;
+
+    return 0;
+} */
+
+
+/* int main() {
+    int x, y;
+    cin >> x;
+    cin >> y;
+
+    int difference = x - y;
+    cout << "Result = " << difference;
+
+    return 0;
+} */
