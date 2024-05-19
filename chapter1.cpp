ec3f1af0424b46d5db641c1ad950949fe899bf12
mahbubur3 <mahbubur9@yahoo.com>
2024-05-19 07:34:38 +0600

    added chapter one

---
+++ chapter1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+
+//! Show output
+/* int main() {
+    // cout << "Hello Mac!";
+
+    int n = 100;
+    long long int n2 = 1599999900000;
+    float n3 = 5.5;
+    double n4 = 5.93333253223;
+    char c = 'M';
+
+    // cout << n << endl; //? endl or \n means same, new line
+    // cout << n2 << endl;
+    // cout << n3 << endl;
+    // cout << n4 << endl;
+    // cout << c << endl;
+
+    cout << "My rank is " << n << endl;
+    cout << "My rank is " << n << " and character is " << c << endl;
+
+    return 0;
+} */
+
+
+//! Take input
+/* int main() {
+    // int n;
+    // cin >> n;
+    // cout << n << endl;
+
+    // Multiple input
+    int a, b;
+    cin >> a >> b;
+    cout << a << " " << b << endl;
+
+    return 0;
+} */
+
+/* int main() {
+    int age;
+    char initial;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter the first letter of your name: ";
+    cin >> initial;
+
+    cout << age << endl;
+    cout << initial << endl;
+
+    return 0;
+} */
+/* int main() {
+    char c;
+    cin >> c;
+    cout << c << endl;
+
+    return 0;
+} */
+
+
