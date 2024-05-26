0f5d967e9e5e92b98c040c8f82a5a05f5e7c7ca9
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-05-26 20:45:14 +0600

    Create chapter_6.cpp

---
+++ chapter_6.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// String constructor
+/*
+int main() {
+    // string = "Hello World";
+    // string s("Hello World");
+    
+    // string s("hello world", 4); // Show 4 characters
+    
+    // string m = "hello world";
+    // string s(m, 4); // Show all characters except first 4
+    
+    string s(10, 'X'); // Print 10 X
+    
+    cout << s << endl;
+    
+    return 0;
+}
+*/
+
+
+// Sort string
+// int main() {
+//     string s;
+//     cin >> s;
+//     sort(s.begin(), s.end());
+//     cout << s << endl;
+    
+//     return 0;
+// }
+
+
+// Range based for loop for string
+/*
+int main() {
+    string s;
+    cin >> s;
+    
+    // Here we find index+element
+    // for (int i = 0; i < s.size(); i++) {
+    //     cout << s[i] << endl;
+    // }
+    
+    // Shortcut - Here we find only element
+    for (char c:s) {
+        cout << c << endl;
+    }
+    // [Note: If we need index then we need to write first loop]
+    
+    return 0;
+}
+*/
