abece766959ee5f71d556dff1b948666f793c942
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-05-26 09:15:27 +0600

    Update codeforces-problems.cpp

--- codeforces-problems.cpp
+++ codeforces-problems.cpp
@@ -67,3 +67,83 @@ int main() {
     
     return 0;
 }
+
+
+// DONE IN LENOVO
+int main() {
+  int test;
+  cin >> test;
+  
+  for (int t = 0; t < test; t++) {
+    int n;
+    cin >> n;
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+    }
+    
+    int sm = INT_MAX;
+    
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int result = a[i] + a[j] + (j + 1) - (i + 1);
+        sm = min(sm, result); // [min] built-it function
+      }
+    }
+    
+    cout << sm << endl;
+  }
+  
+  return 0;
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) {
+                swap(a[i], a[j]);
+            }
+            
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
+}
+
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int count[26] = {0};
+
+    for (int i = 0; i < n; i++) {
+        char ch;
+        cin >> ch;
+        count[ch - 'a']++;
+    }
+
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < count[i]; j++) {
+            cout << char('a' + i);
+        }
+    }
+
+    return 0;
+}
