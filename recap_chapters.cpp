d4e4d6dc9f62ed45467ca5c19d40b2444529856a
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-05-28 19:29:10 +0600

    Create recap_chapters.cpp

---
+++ recap_chapters.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// <------ CHAPTER ONE ------> 
+/* int main() {
+    // char c;
+    // cin >> c;
+    // cout << int(c) << endl;
+
+
+    // double n, m;
+    // cin >> n >> m;
+    
+    // double mul = n * m;
+    // cout << fixed << setprecision(2) << mul;
+
+    return 0;
+} */
+
+
+// <------ CHAPTER TWO ------> 
+/* int main() {
+    // int* n = new int;
+    // cout << n << endl;
+
+    // int* n = new int;
+    // *n = 100;
+    // cout << *n << endl; // value
+    // cout << n << endl;
+
+    // int* n = new int;
+    // *n = 100;
+    // cout << *n << endl;
+    // delete n;
+    // cout << *n << endl;
+
+    return 0;
+} */
+
+/* int* num() {
+    int *n = new int;
+    *n = 500;
+
+    return n;
+}
+
+int main() {
+    int *p = num();
+    // cout << p << endl;
+    cout << *p << endl;
+
+    return 0;
+} */
+
+/* int* numbers(int n) {
+    int *a = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    return a;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int *a = numbers(n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+} */
