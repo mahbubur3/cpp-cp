4f4ffd3a4e1498c07b4804bf59fde02c81cae668
Mahbubur Rahman <mahbubur9@yahoo.com>
2024-06-03 11:50:28 +0600

    Update recap_chapters.cpp

--- recap_chapters.cpp
+++ recap_chapters.cpp
@@ -1,75 +1,88 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
 
-
-// <------ CHAPTER ONE ------> 
 /* int main() {
-    // char c;
-    // cin >> c;
-    // cout << int(c) << endl;
+    int n;
+    cin >> n;
 
-
-    // double n, m;
-    // cin >> n >> m;
-    
-    // double mul = n * m;
-    // cout << fixed << setprecision(2) << mul;
-
-    return 0;
-} */
-
-
-// <------ CHAPTER TWO ------> 
-/* int main() {
-    // int* n = new int;
-    // cout << n << endl;
-
-    // int* n = new int;
-    // *n = 100;
-    // cout << *n << endl; // value
-    // cout << n << endl;
-
-    // int* n = new int;
-    // *n = 100;
-    // cout << *n << endl;
-    // delete n;
-    // cout << *n << endl;
-
-    return 0;
-} */
-
-/* int* num() {
-    int *n = new int;
-    *n = 500;
-
-    return n;
-}
-
-int main() {
-    int *p = num();
-    // cout << p << endl;
-    cout << *p << endl;
-
-    return 0;
-} */
-
-/* int* numbers(int n) {
-    int *a = new int[n];
+    int a[n];
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    return a;
-}
+    int zeroCount = 0, oneCount = 0, twoCount = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            zeroCount++;
+        }
+
+        if (a[i] == 1) {
+            oneCount++;
+        }
+
+        if (a[i] == 0) {
+            twoCount++;
+        }
+    }
+
+    cout << zeroCount << endl;
+    cout << oneCount << endl;
+    cout << twoCount << endl;
+
+    return 0;
+} */
+
+
+/* int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int freq[11] = {0};
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            freq[0]++;
+        }
+
+        if (a[i] == 1) {
+            freq[1]++;
+        }
+
+        if (a[i] == 0) {
+            freq[2]++;
+        }
+    }
+
+    cout << "0 - " << freq[0] << endl;
+    cout << "1 - " << freq[1] << endl;
+    cout << "2 - " << freq[2] << endl;
+
+    return 0;
+} */
+
 
 int main() {
     int n;
     cin >> n;
 
-    int *a = numbers(n);
+    int a[n];
     for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
+        cin >> a[i];
+    }
+
+    int freq[11] = {0};
+    for (int i = 0; i < n; i++) {
+        freq[a[i]]++;
+    }
+
+    for (int i = 0; i < 11; i++) {
+        cout << i << " - " << freq[i] << endl;
     }
 
     return 0;
-} */
+}
